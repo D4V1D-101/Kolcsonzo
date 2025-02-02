106e518772234757d9a9e8a0414abca3f5488f36
D4V1D-101 <115972590+D4V1D-101@users.noreply.github.com>
2025-02-02 22:20:51 +0100

    Add project files.

---
+++ Kolcsonzes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolcsonzo
+{
+    internal class Kolcsonzes
+    {
+        public Kolcsonzes(string r)
+        {
+            var v = r.Split(';');
+            Nev = v[0];
+
+            if (Nev.Contains(','))
+            {
+                var nevekCsoport = Nev.Split(',');
+                var temping = nevekCsoport[0].Trim();
+                nevekCsoport[0] = nevekCsoport[1].Trim();
+                nevekCsoport[1] = temping; 
+      
+                Nev = $"{nevekCsoport[0]}, {nevekCsoport[1]}";
+            }
+          
+
+            Id = int.Parse(v[1]);
+            HajoTipus = v[2];
+            SzemelyekSzama = int.Parse(v[3]);
+            this.elvitelOraja = int.Parse(v[4]);
+            this.elvitelPerce = int.Parse(v[5]);
+            VisszahozatalOraja = int.Parse(v[6]);
+            VisszahozatalPerce = int.Parse(v[7]);
+
+
+
+        }
+
+        
+
+
+        public string Nev { get; set; }
+        public int Id { get; set; }
+        public string HajoTipus { get; set; }
+        public int SzemelyekSzama { get; set; }
+        public int elvitelOraja { get; set; }
+        public int elvitelPerce { get; set; }
+        public int VisszahozatalOraja { get; set; }
+        public int VisszahozatalPerce { get; set; }
+
+        public override string ToString()
+        {
+            return $"";
+        }
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Kolcsonzo;
+
+List <Kolcsonzes> kolcsonzes = new List <Kolcsonzes> ();
+using StreamReader sr = new("../../../src/kolcsonzes.txt", System.Text.Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    kolcsonzes.Add(new Kolcsonzes(sr.ReadLine()));
+}
+foreach (var item in kolcsonzes)
+{
+    Console.WriteLine(item.Nev);
+}
+
+Console.WriteLine("Adjon meg nevet:");
+var k = Console.ReadLine();
+
+
+var kolcsonzo = kolcsonzes.Where(x => x.Nev.Equals(k)).ToList();
+
+if (kolcsonzo.Any())  
+{
+    foreach (var item in kolcsonzo)
+    {
+  
+        Console.WriteLine($"{item.Nev} hajót bérelt {item.elvitelOraja}:{item.elvitelPerce} - {item.VisszahozatalOraja}:{item.VisszahozatalPerce} időtartamban.");
+    }
+}
+else
+{
+    Console.WriteLine("Nem volt ilyen nevű kölcsönző!");
+}
+
+int magyar = 0;
+int kuldoldi = 0;
+foreach (var item in kolcsonzes)
+{
+    if (item.Nev.Contains(','))
+    {
+        kuldoldi++;
+    }
+    else
+    {
+        magyar++;
+    }
+}
+Console.WriteLine($"ennyi magyar van: {magyar}");
+Console.WriteLine($"ennyi külföldi van: {kuldoldi}");
